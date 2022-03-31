b249cb07d5506ae0e009afff572a1263f1ecc2c3
nour <grebici.nour@gmail.com>
2022-03-31 23:29:34 +0100

    home page and login layout

--- src/views/HomeAdmin.vue
+++ src/views/HomeAdmin.vue
@@ -1,6 +1,120 @@
 <script setup></script>
 
 <template>
-  <main><h1>Hello</h1></main>
+  <!-- deviding screen into two columns: sidebar and content -->
+  <el-row>
+    <el-col :span="5">
+      <!-- side menu  -->
+      <aside>
+        <el-scrollbar height="fill">
+          <img src="../assets/logoo.png" class="logo" />
+
+          <el-divider />
+          <el-card :body-style="{ padding: '10px' }">
+            <router-link to="login">login</router-link>
+          </el-card>
+          <el-card :body-style="{ padding: '10px' }">
+            <router-link to="">Home</router-link>
+          </el-card>
+          <el-card :body-style="{ padding: '10px' }">
+            <router-link to="login">login</router-link>
+          </el-card>
+          <el-card :body-style="{ padding: '10px' }">
+            <router-link to="">Home</router-link>
+          </el-card>
+          <el-card :body-style="{ padding: '10px' }">
+            <router-link to="login">login</router-link>
+          </el-card>
+          <el-card :body-style="{ padding: '10px' }">
+            <router-link to="">Home</router-link>
+          </el-card>
+          <el-card :body-style="{ padding: '10px' }">
+            <router-link to="login">login</router-link>
+          </el-card>
+          <el-card :body-style="{ padding: '10px' }">
+            <router-link to="">Home</router-link>
+          </el-card>
+          <el-card :body-style="{ padding: '10px' }">
+            <router-link to="login">login</router-link>
+          </el-card>
+          <el-card :body-style="{ padding: '10px' }">
+            <router-link to="">Home</router-link>
+          </el-card>
+        </el-scrollbar>
+      </aside>
+    </el-col>
+    <el-col :span="19" class="padding">
+      <nav>
+        <!-- nav bar  -->
+        <el-row style="margin-bottom: 10px; margin-top: 10px">
+          <!-- total columns is 24 -->
+          <el-col :span="24">
+            <el-card class="header">
+              <div class="card card-frame col-4">
+                <div class="card-body"></div>
+              </div>
+            </el-card>
+          </el-col>
+        </el-row>
+        <el-row style="margin-bottom: 20px">
+          <el-col :span="12">
+            <!-- body cards  -->
+            <el-card>
+              <div class="card card-frame col-4">
+                <div class="card-body">
+                  This is some text within a card body.
+                </div>
+              </div>
+            </el-card>
+          </el-col>
+          <el-col :span="12">
+            <el-card>
+              <div class="card card-frame col-4">
+                <div class="card-body">
+                  This is some text within a card body.
+                </div>
+              </div>
+            </el-card>
+          </el-col>
+        </el-row>
+      </nav>
+    </el-col>
+  </el-row>
 </template>
-<style></style>
+<style>
+aside {
+  height: 100vh;
+  background-color: rgb(255, 251, 248);
+}
+.padding .el-col {
+  padding: 0px 10px;
+}
+.logo {
+  width: 60%;
+  height: 10%;
+  margin: 11% auto;
+  display: flex;
+}
+.header {
+  height: 19vh;
+  background-color: var(--vt-c-orange);
+}
+li {
+  list-style: none;
+  text-decoration: none;
+  color: black;
+}
+.el-scrollbar .el-card {
+  border-radius: 10px;
+  margin: 4% 10%;
+  text-align: center;
+}
+a {
+  text-decoration: none;
+  color: black;
+}
+.el-divider {
+  margin: 0;
+  padding-bottom: 10px;
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,70 @@
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+};
+</script>
 <template>
-  <div class="login">
-    <h1>This is a login page</h1>
-  </div>
+  <el-card class="head"> </el-card>
+  <el-card class="form">
+    <img src="../assets/logoo.png" class="logoLogin" />
+    <form action="" method="">
+      <label for="email">Email</label>
+      <input
+        type="email"
+        v-model.trim="email"
+        name="email"
+        id="email"
+        placeholder="example@gmail.com"
+      />
+      <br />
+      <label for="password">password</label>
+      <input
+        type="password"
+        v-model="password"
+        name="password"
+        id="password"
+        placeholder="*********"
+      />
+      <br />
+      <input type="submit" value="Sign In" />
+    </form>
+  </el-card>
 </template>
 
-<style></style>
+<style>
+.head {
+  background-color: var(--vt-c-orange);
+  height: 30vh;
+}
+.form {
+  background-color: rgba(255, 255, 255, 0.767);
+  height: 70vh;
+  width: 40%;
+  position: relative;
+  margin: 0 auto;
+  top: -20vh;
+  border-radius: 20px;
+}
+label {
+  display: block;
+  padding-left: 4%;
+}
+input {
+  width: 80%;
+  padding: 2%;
+  margin: 1% auto;
+  display: flex;
+}
+.logoLogin {
+  width: 40%;
+  height: 20%;
+  margin: 0 auto;
+  display: flex;
+  margin-bottom: 10%;
+}
+</style>
