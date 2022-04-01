19de8da29324cdaff54e5a2dd618471b7fc69e3c
nour <grebici.nour@gmail.com>
2022-04-01 21:46:22 +0100

    routing control fixing + pageNotFound route

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeAdmin.vue";
 import LoginView from "../views/LoginView.vue";
+import NotFound from "../views/NotFound.vue";
+
 function CheckLogin(to, from, next) {
   var isAuthenticated = false;
   if (localStorage.getItem("LoggedUser")) isAuthenticated = true;
   else isAuthenticated = false;
+  console.log("auth: " + isAuthenticated);
+  console.log("to name: " + (to.name == "login"));
   if (isAuthenticated && to.name !== "login") {
     next();
-  } else if (!isAuthenticated) {
-    next("/login");
-  } else {
+  } else if (to.name == "login" && !isAuthenticated) {
+    next();
+  } else if (isAuthenticated) {
     next("/");
-  }
+  } else next("/login");
 }
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +31,11 @@ const router = createRouter({
       name: "login",
       component: LoginView,
       beforeEnter: CheckLogin,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "notFound",
+      component: NotFound,
     },
   ],
 });
