2030dc9821e8ce2a33255fdda0a26c3ce94717fd
nour <grebici.nour@gmail.com>
2022-06-15 10:57:02 +0100

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -54,12 +54,6 @@ export default defineConfig({
         secure: false,
         rewrite: (path) => path.replace(/^\/home/, ""),
       },
-      // "/home/api/admin": {
-      //   target: "http://127.0.0.1:5001",
-      //   changeOrigin: true,
-      //   secure: false,
-      //   rewrite: (path) => path.replace(/^\/home/, ""),
-      // },
       "/home/api/admin/Category": {
         target: "http://127.0.0.1:5002",
         changeOrigin: true,
@@ -68,6 +62,12 @@ export default defineConfig({
       },
       "/api/admin/subCategory":{
         target: "http://127.0.0.1:5002",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/home/, ""),
+      },
+      "/home/api/admin": {
+        target: "http://127.0.0.1:5001",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/home/, ""),
